2b1108564204e51d4f05d89f77845f039ddf1841
JeanDeCarli <jean.carlo.jc1@gmail.com>
2015-11-18 20:52:31 -0200

    Implemented methods to get and save the information on DB to Framework solution

--- TestReport.Framework/Framework/DataBaseMethods/DbMethods.cs
+++ TestReport.Framework/Framework/DataBaseMethods/DbMethods.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Framework.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -7,18 +8,30 @@ using System.Threading.Tasks;
 
 namespace Framework.DataBaseMethods
 {
-    public class DbMethods : DbContext
+    public class DbMethods
     {
+        private TestReportEntities db = new TestReportEntities();
+
         public int getStatusIdByName(string statusName)
         {
-            // todo fazer implementação
-            return 0;
+            // get the status line from database where status name is equals to parameter
+            var status = db.Status.Where(s => s.name.Equals(statusName)).FirstOrDefault();
+            // return the ID of the status returned from DB
+            return status.id;
         }
 
         public int getProjectsIdByName(string projectsName)
         {
-            // todo fazer implementação
-            return 0;
+            // get the project`s line from database where project`s name is equals to parameter
+            var project = db.Projects.Where(p => p.name.Equals(projectsName)).FirstOrDefault();
+            // return the ID of the status returned from DB
+            return project.id;
+        }
+
+        public void SaveExecution(Execution exec)
+        {
+            // save a new line in the table Execution
+            db.Executions.Add(exec);
         }
     }
 }
--- TestReport.Framework/Framework/FrameworkMethods/FrameworkMethods.cs
+++ TestReport.Framework/Framework/FrameworkMethods/FrameworkMethods.cs
@@ -13,7 +13,7 @@ namespace Framework
 {
     public class FrameworkMethods
     {
-        private Stopwatch watch = new Stopwatch();
+        private Stopwatch stopWatch = new Stopwatch();
 
         public void CaptureTestsInformation()
         {
@@ -21,32 +21,43 @@ namespace Framework
             var context =  TestContext.CurrentContext;
             var currentTestResult = new Execution();
             var db = new DbMethods();
+
             // get the runned test`s values
             string[] testsFullName = context.Test.FullName.Split('.');
+
+            string testName = testsFullName[2];
+            int idStatus = db.getStatusIdByName(context.Result.Status.ToString());
+
+            //int idPhase = 
+
+            TimeSpan executionTime = this.stopTimer();
+            DateTime executionDate = System.DateTime.Now;
             int idProject = db.getProjectsIdByName(testsFullName[0]);
-
-
-            // set the information in the Execution Model
-            currentTestResult.testName = testsFullName[2];
             
-            this.SaveTestResult(currentTestResult);
+            // set the information in the Execution Model
+            currentTestResult.testName = testName;
+            currentTestResult.idStatus = idStatus;
+
+            //currentTestResult.idPhase = ...
+
+            currentTestResult.executionTime = executionTime;
+            currentTestResult.executionDate = executionDate;
+            currentTestResult.idProject = idProject;
+
+            // call the method to save the Execution Object
+            db.SaveExecution(currentTestResult);
             //System.Diagnostics.Debug.Write(currentTestResult.testsName + " " + currentTestResult.testsStatus + " " + currentTestResult.testsTime + " " + currentTestResult.testsDate);
         }
 
         public void startTimer()
         {
-            this.watch.Start();
+            stopWatch.Start();
         }
 
-        private string stopTimer()
+        private TimeSpan stopTimer()
         {
-            var time = watch.ElapsedMilliseconds;
-            return time.ToString();
-        }
-
-        private void SaveTestResult(Execution exec)
-        {
-            // salva no db.
+            stopWatch.Stop();
+            return stopWatch.Elapsed;
         }
     }
 }
